a4040b3f744dc98db9f5f561b77497239061ef05
Efrain May <efrain_may_75@hotmail.com>
2021-01-13 21:09:29 -0600

    se agrego eñl marcador en google maps para poder obtener los respectivos marcadores en el mapa de cada una de las geolocalizaciones2

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -24,6 +24,13 @@ class _MapaPageState extends State<MapaPage> {
       tilt: 50
     );
 
+    //marcadores
+    Set<Marker> markers = new Set<Marker>();
+    markers.add(new Marker(
+      markerId: MarkerId('geo-location'),
+      position: scan.getLatLng()
+    ));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapa'),
@@ -31,6 +38,7 @@ class _MapaPageState extends State<MapaPage> {
       body: GoogleMap(
         myLocationButtonEnabled: false,
         mapType: MapType.normal,
+        markers: markers,
         initialCameraPosition: puntoInical,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
